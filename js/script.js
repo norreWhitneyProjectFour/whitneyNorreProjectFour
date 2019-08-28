9fc96c57c1a377684ae8fb13983e1ba1eebd48cc
Norre Daroy <norrej.daroy@gmail.com>
2019-08-28 10:01:54 -0400

    Added function for user to select a date :calendar:, city :school: and display :tv: the events

--- js/script.js
+++ js/script.js
@@ -7,27 +7,20 @@ eventsApp.url = "https://app.ticketmaster.com/discovery/v2/events";
 
 eventsApp.apiKey = "ftYfSGG92vqF6hHoXIE25YwqEXwj0jhe";
 
-eventsApp.getEvents = () => {
-  // $.ajax({
-  //   url: eventsApp.url,
-  //   method: 'GET',
-  //   datatype: 'json',
-  //   data: {
-  //     key: eventsApp.apiKey,
-  //     city: 'Toronto',
-  //     startDateTime: '2019-09-13'
-  //   }
-  // }).then((res) => {
-  //   console.log("IT WORKED!");
-  // })
+eventsApp.userPickDate = '';
+eventsApp.userPickCity = '';
+eventsApp.eventsArray = [];
 
+eventsApp.getEvents = (city, date) => {
   $.ajax({
     type: "GET",
-    url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=KA2M6AWn63bg3pVc9OkXqcDPqV2x2Dbc&city=toronto&startDateTime=2019-08-29T00:00:00Z&endDateTime=2019-08-29T23:00:00Z",
+    url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=KA2M6AWn63bg3pVc9OkXqcDPqV2x2Dbc&city=${city}&startDateTime=${date}T00:00:00Z&endDateTime=${date}T23:00:00Z`,
     async: true,
     dataType: "json",
+    
     success: function (res) {
       console.log(res);
+      eventsApp.displayEvents(res);
       // Parse the response.
       // Do other things.
     },
@@ -36,11 +29,47 @@ eventsApp.getEvents = () => {
     }
   });
 }
+eventsApp.getUserInput = () => {
+  //Gets the value of selected city
+  $('select').on('change', function () {
+    eventsApp.userPickCity = $('select').val();
+  });
+  //Gets value of selected date
+  $('input').on('change', function() {
+    eventsApp.userPickDate = $('input').val();
+    eventsApp.getEvents(eventsApp.userPickCity, eventsApp.userPickDate);
+  });
+  
+}
+eventsApp.displayEvents = (result) => {
+  for(let i = 0; i <= 3; i++) {
+    console.log(result._embedded.events[i].name);
+    // console.log(eventsArray);
+  }
+  
+  //   if (piece.hasImage) {
+  //     const htmlToPost = (`<div class="piece">
+  //               <h2>${piece.title}</h2>
+  //               <p class="artist">${piece.principalOrFirstMaker}</p>
+  //               <img src="${piece.webImage.url}" alt="${piece.longTitle}"></div>`);
 
+  //     $('#artwork').append(htmlToPost);
+  //   }
+  // });
+}
+//Function to display info on hovered text
+eventsApp.hoverInfo = () => {
+  $('.hover').on('mouseover', function (){
+    const hover = $('.hover').text();
+    console.log(hover);
+  })
+}
 //create document ready
 $(document).ready(function () {
-  eventsApp.getEvents();
+  eventsApp.getUserInput();
+  eventsApp.hoverInfo();
 });
+
 
 //store the API data in an array
 //if event is happening display events using, if no events happening use .fail() to display "no events happening today"
