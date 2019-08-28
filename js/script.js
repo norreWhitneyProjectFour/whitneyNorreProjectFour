e11a44ceada8d121bcc94b2a1386ea4a95ac11ac
Norre Daroy <norrej.daroy@gmail.com>
2019-08-28 18:14:19 -0400

    Filtered and display 3 events onto HTML page

--- js/script.js
+++ js/script.js
@@ -7,25 +7,14 @@ eventsApp.url = "https://app.ticketmaster.com/discovery/v2/events";
 
 eventsApp.apiKey = "ftYfSGG92vqF6hHoXIE25YwqEXwj0jhe";
 
-<<<<<<< HEAD
-eventsApp.userPickDate = '';
-eventsApp.userPickCity = '';
-eventsApp.eventsArray = [];
+eventsApp.userPickDate = '',
+eventsApp.userPickCity = '',
+eventsApp.eventsArray = [],
 
 eventsApp.getEvents = (city, date) => {
   $.ajax({
     type: "GET",
     url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=KA2M6AWn63bg3pVc9OkXqcDPqV2x2Dbc&city=${city}&startDateTime=${date}T00:00:00Z&endDateTime=${date}T23:00:00Z`,
-=======
-eventsApp.date = "2019-08-29T23:00:00Z";
-
-eventsApp.getEvents = (date) => {
-
-  //store an AJAX request in a variable called eventsApp.getEvents, we are storing a promise in a variable:
-  $.ajax({
-    type: "GET",
-    url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=KA2M6AWn63bg3pVc9OkXqcDPqV2x2Dbc&city=toronto&startDateTime=2019-08-29T00:00:00Z&endDateTime=${date}`,
->>>>>>> 337b4d536cd2f6600276885ddd0ad67848424529
     async: true,
     dataType: "json",
     
@@ -36,10 +25,11 @@ eventsApp.getEvents = (date) => {
       // Do other things.
     },
     error: function (xhr, status, err) {
+      console.log("NO EVENTS!!");
       // This time, we do not end up here!
     }
   });
-}
+},
 eventsApp.getUserInput = () => {
   //Gets the value of selected city
   $('select').on('change', function () {
@@ -53,21 +43,19 @@ eventsApp.getUserInput = () => {
   
 }
 eventsApp.displayEvents = (result) => {
-  for(let i = 0; i <= 3; i++) {
-    console.log(result._embedded.events[i].name);
-    // console.log(eventsArray);
+  $('.displayEvents').empty();
+  for(let i = 0; i < 3; i++) {
+    
+    $('.displayEvents').append(`<div>
+                                    <h2>${result._embedded.events[i].name}</h2>
+                                    <p>${result._embedded.events[i]._embedded.venues[0].name}</p>
+                                    <p>${result._embedded.events[i].dates.start.localTime}</p>
+                                    <img src="${result._embedded.events[i].images[4].url}" alt=""/>
+                                    <a href="${result._embedded.events[i].url}">get tickets</a>
+                                    </div>`);
+   
   }
   
-  //   if (piece.hasImage) {
-  //     const htmlToPost = (`<div class="piece">
-  //               <h2>${piece.title}</h2>
-  //               <p class="artist">${piece.principalOrFirstMaker}</p>
-  //               <img src="${piece.webImage.url}" alt="${piece.longTitle}"></div>`);
-
-<<<<<<< HEAD
-  //     $('#artwork').append(htmlToPost);
-  //   }
-  // });
 }
 //Function to display info on hovered text
 eventsApp.hoverInfo = () => {
@@ -80,11 +68,6 @@ eventsApp.hoverInfo = () => {
 $(document).ready(function () {
   eventsApp.getUserInput();
   eventsApp.hoverInfo();
-=======
-// wait for the document to be ready
-$(document).ready(function () {
-  eventsApp.getEvents(eventsApp.date);
->>>>>>> 337b4d536cd2f6600276885ddd0ad67848424529
 });
 
 
