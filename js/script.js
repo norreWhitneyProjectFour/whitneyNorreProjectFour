6d7c94d3c431133356276a62ca8280b08ea9ecfb
Whitney Reid <whitreid@hotmail.com>
2019-08-29 09:47:47 -0400

    revised header and calendar 👍🔥😎

--- js/script.js
+++ js/script.js
@@ -8,44 +8,44 @@ eventsApp.url = "https://app.ticketmaster.com/discovery/v2/events";
 eventsApp.apiKey = "ftYfSGG92vqF6hHoXIE25YwqEXwj0jhe";
 
 eventsApp.userPickDate = '',
-eventsApp.userPickCity = '',
-eventsApp.eventsArray = [],
+  eventsApp.userPickCity = '',
+  eventsApp.eventsArray = [],
 
-eventsApp.getEvents = (city, date) => {
-  $.ajax({
-    type: "GET",
-    url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=KA2M6AWn63bg3pVc9OkXqcDPqV2x2Dbc&city=${city}&startDateTime=${date}T00:00:00Z&endDateTime=${date}T23:00:00Z`,
-    async: true,
-    dataType: "json",
-    
-    success: function (res) {
-      console.log(res);
-      eventsApp.displayEvents(res);
-      // Parse the response.
-      // Do other things.
-    },
-    error: function (xhr, status, err) {
-      console.log("NO EVENTS!!");
-      // This time, we do not end up here!
-    }
-  });
-},
-eventsApp.getUserInput = () => {
-  //Gets the value of selected city
-  $('select').on('change', function () {
-    eventsApp.userPickCity = $('select').val();
-  });
-  //Gets value of selected date
-  $('input').on('change', function() {
-    eventsApp.userPickDate = $('input').val();
-    eventsApp.getEvents(eventsApp.userPickCity, eventsApp.userPickDate);
-  });
-  
-}
+  eventsApp.getEvents = (city, date) => {
+    $.ajax({
+      type: "GET",
+      url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=KA2M6AWn63bg3pVc9OkXqcDPqV2x2Dbc&city=${city}&startDateTime=${date}T00:00:00Z&endDateTime=${date}T23:00:00Z`,
+      async: true,
+      dataType: "json",
+
+      success: function (res) {
+        console.log(res);
+        eventsApp.displayEvents(res);
+        // Parse the response.
+        // Do other things.
+      },
+      error: function (xhr, status, err) {
+        console.log("NO EVENTS!!");
+        // This time, we do not end up here!
+      }
+    });
+  },
+  eventsApp.getUserInput = () => {
+    //Gets the value of selected city
+    $('select').on('change', function () {
+      eventsApp.userPickCity = $('select').val();
+    });
+    //Gets value of selected date
+    $('input').on('change', function () {
+      eventsApp.userPickDate = $('input').val();
+      eventsApp.getEvents(eventsApp.userPickCity, eventsApp.userPickDate);
+    });
+
+  }
 eventsApp.displayEvents = (result) => {
   $('.displayEvents').empty();
-  for(let i = 0; i < 3; i++) {
-    
+  for (let i = 0; i < 3; i++) {
+
     $('.displayEvents').append(`<div>
                                     <h2>${result._embedded.events[i].name}</h2>
                                     <p>${result._embedded.events[i]._embedded.venues[0].name}</p>
@@ -53,13 +53,13 @@ eventsApp.displayEvents = (result) => {
                                     <img src="${result._embedded.events[i].images[4].url}" alt=""/>
                                     <a href="${result._embedded.events[i].url}">get tickets</a>
                                     </div>`);
-   
+
   }
-  
+
 }
 //Function to display info on hovered text
 eventsApp.hoverInfo = () => {
-  $('.hover').on('mouseover', function (){
+  $('.hover').on('mouseover', function () {
     const hover = $('.hover').text();
     console.log(hover);
   })
@@ -68,6 +68,25 @@ eventsApp.hoverInfo = () => {
 $(document).ready(function () {
   eventsApp.getUserInput();
   eventsApp.hoverInfo();
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  $('.myCalendar').calendar({
+    select: function (date) {
+      console.log('SELECT', date)
+    }
+  });
+
 });
 
 
